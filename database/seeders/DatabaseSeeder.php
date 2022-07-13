82a69f4087a00d14e0b9beb1677b1f6f386365a3
moacirx <moacir.crica@gmail.com>
2022-07-12 22:39:06 -0300

    Instalado adminlte3 e tradução pt-br e vue e auth

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(100)->create();
+
+        \App\Models\User::factory()->create([
+            'name' => 'Moacir de Oliveira',
+            'email' => 'moacir.crica@gmail.com',
+        ]);
     }
 }
